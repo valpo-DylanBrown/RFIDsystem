1fd8a2bff5d15133354a86d488ae1693ff66e0ae
Dylan Brown <dylan.brown@valpo.edu>
2019-02-12 17:32:19 -0600

    beta

--- code/esp-rfid-http/sketch_feb12b/sketch_feb12b.ino
+++ code/esp-rfid-http/sketch_feb12b/sketch_feb12b.ino
@@ -1,22 +1,40 @@
-
-
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <SoftwareSerial.h> // communication between ESP and RFID reader
-#include "include/SparkFun_UHF_RFID_Reader.h"
-#include "include/util.h"
-#include "include/WiFi_Config.h"
-#include "include/RFID_Config.h"
+#include "SparkFun_UHF_RFID_Reader.h" //Library for controlling the M6E Nano module
+#include "util.h"
 
-#include <elapsedMillis.h> //https://playground.arduino.cc/Code/ElapsedMillis
+#include <elapsedMillis.h>
 uint16_t interval = 15000;
 
-//To enable debugging for RFID, set to 1
-#define RFID_DEBUGGING 0
+// Ensure the ESP shield 5V pin is NOT connected to the RFID shield.
+//   Clip off the pin is an easy but "permanent" solution.
+//   This should already have been done.
 
-//GE-100 default network
-char ssid[] = "VU-Media";
-char password[] = "";  // unused for unencrypted connection
+// Use a male-female jumper wire to connect the ESP's 5V pin to the RFID shield's
+//   external power input (+) pin.
+
+// jumper wires from:
+//   GPIO 13 to shield 2
+//   GPIO 15 to shield 3
+// select "SW-UART" on RFID board
+// move UART switch to "HW" on the ESP board
+
+
+// For the buzzer, connect these pins between the ESP and RFID reader:
+//   GPIO 12 to shield 9
+//   GPIO 14 to shield 10
+#define BUZZER1 12
+#define BUZZER2 14
+
+
+const char ssid[] = "VU-Media";
+const char password[] = "";  // unused for unencrypted connection
+
+
+// Create instances of the serial driver and the RFID driver
+SoftwareSerial softSerial(13, 15); //RX, TX (from ESP's view)
+RFID nano;
 
 // instances of the WiFi and network drivers
 WiFiClient client;
@@ -29,15 +47,58 @@ void setup()
 
   Serial.begin(115200);  // Serial monitor must match this rate
 
+  // Setup the pins for the buzzer
+  pinMode(BUZZER1, OUTPUT);
+  pinMode(BUZZER2, OUTPUT);
+  digitalWrite(BUZZER2, LOW); //Pull half the buzzer to ground and drive the other half.
+
   while (!Serial);  //wait until open
 
-  //Initialize RFID
-  RFIDInit(RFID_DEBUGGING);
+  // now get the RFID reader setup
+  Serial.println();
+  Serial.println("Initializing RFID reader...");
 
-  //Initialize WiFi
-  wifiInit(ssid); //Since VU-Media is unencrypted, a password is not needed
-  
+  // nano defaults to 115200 baud rate, keep it the same!
+  softSerial.begin(115200);
+  while (!softSerial);  // wait until open
+
+  if (setupNano(nano, softSerial) == false)
+  {
+    Serial.println("Module failed to initialize. Check wiring?");
+    status(ERROR);
+    delay(1000);
+    Serial.println("Restarting...");
+    ESP.restart();
+    while (1); //Freeze!
+  }
+
+  nano.setReadPower(500); //5.00 dBm. Higher values may cause USB port to brown out
+  //Max Read TX Power is 27.00 dBm and may cause temperature-limit throttling
+
+  // Uncomment this to see the commands and responses sent/received
+  //   between the ESP and Reader
+  //nano.enableDebugging(Serial);
   status(GOOD);
+
+  
+  // Setup client-only mode, "station"
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+
+  WiFi.begin(ssid);  // for unencrypted SSID
+  //WiFi.begin(ssid, password);  // encrypted SSIDs
+
+  while (WiFi.status() != WL_CONNECTED) {
+    status(WAITING);
+    Serial.println(WiFi.status());
+  }
+
+  Serial.println("");
+  WiFi.printDiag(Serial);
+  Serial.println("WiFi connected");  
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  status(GOOD);  // done with setup
 }
 
 
@@ -52,24 +113,19 @@ void loop()
   byte responseType = 0;
   byte count = 0;
 
-  while (responseType != RESPONSE_SUCCESS)//RESPONSE_IS_TAGFOUND
+  while (responseType != RESPONSE_SUCCESS)//RESPONSE_IS_TAGFOUND)
   {
     myEPClength = sizeof(myEPC); //Length of EPC is modified each time .readTagEPC is called
 
     responseType = nano.readTagEPC(myEPC, myEPClength, 500); //Scan for a new tag up to 500ms
-    
-    if (count == 0){
+    if (count==0){
       Serial.println(F("Searching for tag"));
       count++;
     }
-    
-    //Serial.println(count); //For debugging purposes
-
+    //Serial.println(count); // debug
     delay(500);
   }
-
-  count = 0; //Reset count for RESPONSE while loop
-  
+  count = 0; //reset for RESPONSE while loop
   //Beep! Piano keys to frequencies: http://www.sengpielaudio.com/KeyboardAndFrequencies.gif
   tone(BUZZER1, 2093, 150); //C
   delay(150);
@@ -90,7 +146,7 @@ void loop()
 
 
   const char* host = "api.thingspeak.com";
-  const char* streamID = "ZGUH9PMYN82B6TY4";
+  const char* streamID = "8BZUUR7S8I924EN5";
   // Example: GET https://api.thingspeak.com/update?api_key=8X26QMKV55Q1LTDK&field1=
 
   String url = "http://";
@@ -118,9 +174,10 @@ void loop()
   int httpCode = http.GET();
   String response = http.getString();
   http.end();
+
   Serial.print("HTTP response code: ");
   Serial.println(httpCode);
-  Serial.println(response); 
+  Serial.println(response);
 
   responseType = 0; //Reset responseType
   elapsedMillis timeElapsed;
@@ -153,6 +210,4 @@ void loop()
   timeElapsed = 0; //Reset the elapsed time
 
   status(GOOD);
-
-  
 }
