86d04b28787896a402ac0a5d0bf7dc0b0e2cc2a5
Dylan Brown <dylan.brown@valpo.edu>
2019-02-22 15:00:30 -0600

    matlab updates

--- matlab/beta_Filtering_ver4.m
+++ matlab/beta_Filtering_ver4.m
@@ -35,8 +35,12 @@ newdata = table2timetable(data);
 % NOTE THIS DOES NOT ACCOUNT FOR DAYLIGHT SAVINGS TIME
 % The "-0.5" subtracts 12 hours from the current datetime. This number can
 % be shortened or lengthed depending on needs of clients.
-S = timerange((datetime('now')+0.21)-0.5, datetime('now'));
-
+%S = timerange((datetime('now')+0.21)-0.5, datetime('now'));
+% FIXED HERE
+hoursUntilNotPresent = 12;
+t1 = datetime('now')-hours(hoursUntilNotPresent);
+t2 = datetime('now');
+S=timerange(t1, t2);
 
 %Creates new timetable with the filtered time range S and rfid
 filteredData = newdata(S,:);
@@ -58,8 +62,9 @@ T.Properties.RowNames = tagIDs;
 
 % Add column called "Present" and set to zero
 T.Present = zeros(height(T), 1);
-% Add a columnt called "Date" and put today's date in
+% Add a column called "Date" and put today's date in
 T.Date = repmat({datestr(now, 02)}, height(T), 1); 
+% add a column called "time" and put the time a student checked in
 
 % For every filtered tag ID we've found, mark the student present
 %  This is accomplished by setting the 'Present' field to 1
@@ -82,7 +87,8 @@ Tout = T(:, {'lastName', 'firstName', 'Present', 'Date'});
 % Write this output to a file 
 % Can be outputted to a text (.txt, .dat, .csv) file or 
 % Excel (xls,.xlsm,.xlsx) file
-writetable(Tout, 'out.xlsx');
+writetable(Tout, 'out.xlsx', 'FileType','spreadsheet','Sheet','Attendance');
+warning( 'off', 'MATLAB:xlswrite:AddSheet' ) ;
 %%opens the spreadsheet depending on system
 if ismac
     system('open out.xlsx');
@@ -99,4 +105,5 @@ end
 % Author - Dylan Brown
 % This code is used in part with SparkFun ESP8266 and Arduino to filter
 % RFID tags from a database into a classroom attendance system
-% Others Involved: Ahmed Almousa, Matthew Bickelhaupt, & Chris Gutschlag
+% Others Involved: Daniel Piccoli and Lindsey Tryban
+% Advisor - Dan White
